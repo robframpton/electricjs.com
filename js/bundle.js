0f5f5b42bd287102cff0628f34bc656c30ee7be0
Robert-Frampton <ROBERT.FRAMPTON@LIFERAY.COM>
2017-01-20 09:58:22 -0800

    Update 2017-01-20T17:58:20.943Z

--- js/bundle.js
+++ js/bundle.js
@@ -21529,7 +21529,7 @@ babelHelpers;
   var templates;
   goog.loadModule(function (exports) {
 
-    // This file was automatically generated from SideBar.soy.
+    // This file was automatically generated from Sidebar.soy.
     // Please don't edit this file by hand.
 
     /**
@@ -21673,129 +21673,7 @@ babelHelpers;
 
 	Soy.register(Sidebar, templates);
 
-	this['metal']['SideBar'] = Sidebar;
-}).call(this);
-'use strict';
-
-(function () {
-  /* jshint ignore:start */
-  var Component = this['metal']['component'];
-  var Soy = this['metal']['Soy'];
-
-  var templates;
-  goog.loadModule(function (exports) {
-
-    // This file was automatically generated from SideBar.soy.
-    // Please don't edit this file by hand.
-
-    /**
-     * @fileoverview Templates in namespace Sidebar.
-     * @hassoydeltemplate {ElectricNavigation.anchor.idom}
-     * @public
-     */
-
-    goog.module('Sidebar.incrementaldom');
-
-    /** @suppress {extraRequire} */
-    var soy = goog.require('soy');
-    /** @suppress {extraRequire} */
-    var soydata = goog.require('soydata');
-    /** @suppress {extraRequire} */
-    goog.require('goog.i18n.bidi');
-    /** @suppress {extraRequire} */
-    goog.require('goog.asserts');
-    /** @suppress {extraRequire} */
-    goog.require('goog.string');
-    var IncrementalDom = goog.require('incrementaldom');
-    var ie_open = IncrementalDom.elementOpen;
-    var ie_close = IncrementalDom.elementClose;
-    var ie_void = IncrementalDom.elementVoid;
-    var ie_open_start = IncrementalDom.elementOpenStart;
-    var ie_open_end = IncrementalDom.elementOpenEnd;
-    var itext = IncrementalDom.text;
-    var iattr = IncrementalDom.attr;
-
-    var $templateAlias2 = Soy.getTemplate('ElectricNavigation.incrementaldom', 'render');
-
-    var $templateAlias1 = Soy.getTemplate('ElectricSearchAutocomplete.incrementaldom', 'render');
-
-    /**
-     * @param {Object<string, *>=} opt_data
-     * @param {(null|undefined)=} opt_ignored
-     * @param {Object<string, *>=} opt_ijData
-     * @return {void}
-     * @suppress {checkTypes}
-     */
-    function $render(opt_data, opt_ignored, opt_ijData) {
-      ie_open('nav', null, null, 'class', 'sidebar');
-      ie_open('a', null, null, 'class', 'sidebar-header toggler-header-collapsed');
-      ie_void('span', null, null, 'class', 'sidebar-icon icon-16-menu');
-      ie_open('span');
-      itext('Docs Menu');
-      ie_close('span');
-      ie_open('span', null, null, 'class', 'sidebar-icon-right');
-      ie_void('span', null, null, 'class', 'icon-12-arrow-down-short');
-      ie_void('span', null, null, 'class', 'icon-12-arrow-up-short');
-      ie_close('span');
-      ie_close('a');
-      ie_open('div', null, null, 'class', 'sidebar-toggler-content toggler-collapsed');
-      ie_open('div', null, null, 'class', 'sidebar-search');
-      $templateAlias1({ section: opt_data.section, placeholder: 'Search Docs' }, null, opt_ijData);
-      ie_close('div');
-      $templateAlias2({ elementClasses: 'sidebar-list sidebar-list-1', listItemClasses: 'sidebar-item', anchorVariant: 'sidebar', section: opt_data.section }, null, opt_ijData);
-      ie_close('div');
-      ie_close('nav');
-    }
-    exports.render = $render;
-    if (goog.DEBUG) {
-      $render.soyTemplateName = 'Sidebar.render';
-    }
-
-    /**
-     * @param {Object<string, *>=} opt_data
-     * @param {(null|undefined)=} opt_ignored
-     * @param {Object<string, *>=} opt_ijData
-     * @return {void}
-     * @suppress {checkTypes}
-     */
-    function __deltemplate_s14_d34389eb(opt_data, opt_ignored, opt_ijData) {
-      ie_open('a', null, null, 'class', 'sidebar-link ' + (opt_data.page.active ? 'sidebar-link-selected' : ''), 'href', opt_data.page.url);
-      ie_void('span', null, null, 'class', 'sidebar-icon icon-16-' + opt_data.page.icon);
-      ie_open('span');
-      var dyn0 = opt_data.page.title;
-      if (typeof dyn0 == 'function') dyn0();else if (dyn0 != null) itext(dyn0);
-      ie_close('span');
-      ie_close('a');
-    }
-    exports.__deltemplate_s14_d34389eb = __deltemplate_s14_d34389eb;
-    if (goog.DEBUG) {
-      __deltemplate_s14_d34389eb.soyTemplateName = 'Sidebar.__deltemplate_s14_d34389eb';
-    }
-    soy.$$registerDelegateFn(soy.$$getDelTemplateId('ElectricNavigation.anchor.idom'), 'sidebar', 0, __deltemplate_s14_d34389eb);
-
-    exports.render.params = ["section"];
-    exports.render.types = { "section": "any" };
-    templates = exports;
-    return exports;
-  });
-
-  var Sidebar = function (_Component) {
-    babelHelpers.inherits(Sidebar, _Component);
-
-    function Sidebar() {
-      babelHelpers.classCallCheck(this, Sidebar);
-      return babelHelpers.possibleConstructorReturn(this, (Sidebar.__proto__ || Object.getPrototypeOf(Sidebar)).apply(this, arguments));
-    }
-
-    return Sidebar;
-  }(Component);
-
-  Soy.register(Sidebar, templates);
-  this['metalNamed']['SideBar'] = this['metalNamed']['SideBar'] || {};
-  this['metalNamed']['SideBar']['Sidebar'] = Sidebar;
-  this['metalNamed']['SideBar']['templates'] = templates;
-  this['metal']['SideBar'] = templates;
-  /* jshint ignore:end */
+	this['metal']['Sidebar'] = Sidebar;
 }).call(this);
 'use strict';
 
